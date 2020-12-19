d21e94be9be1b2e4c9e737f336dcf0f002832f4c
VIKRAM KUMAR <vkbiotech841@gmail.com>
2020-12-19 12:03:31 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'replicate-forms-angular';
+
+  orderForm: FormGroup;
+  items: FormArray;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
+
+
+  ngOnInit() {
+    this.orderForm = new FormGroup({
+      items: new FormArray([])
+    });
+  }
+
+
+
+  createItem(): FormGroup {
+    return this.formBuilder.group({
+      name: '',
+      description: '',
+      price: ''
+    });
+  };
+
+
+  addItem(): void {
+    this.items = this.orderForm.get('items') as FormArray;
+    this.items.push(this.createItem());
+  };
+
 }
